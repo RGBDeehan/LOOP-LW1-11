832690afce118fe0680c1b83a9735daa848f1cdb
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-17 16:23:19 +0600

    Update loopTJ1.c

--- loopTJ1.c
+++ loopTJ1.c
@@ -1,3 +1,5 @@
+/* Write a C program which can input some persons’ ages and display how many of them are teenagers. Sample Input 7 12 19 24 11 17 13 65 Sample Output Teenage: 3 */
+
 #include <stdio.h>
 int main(){
 int x , y , c;
